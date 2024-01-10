b86df8f37de010d88d82f54e8c4e9bcf7a3f2421
fadliromi <154489463+fadliromi@users.noreply.github.com>
2024-01-10 17:00:34 +0700

    Add files via upload

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>10</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b3821c60-86e8-47f3-bdce-3607163b7ea5</testSuiteGuid>
+   <testCaseLink>
+      <guid>a100dacf-2164-4fac-bce3-0f66c23b14c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Stockbit/GetBrokSumPriceDaily</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
